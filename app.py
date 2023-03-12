af5f9875735fc57baa4600db20faae89f7f9e137
Chelsea Griffo <chelseagriffo@gmail.com>
2023-03-12 12:20:54 -0500

    Add app variable, 2 decorators, and dunder main

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+#decorator
+@app.route('/')
+def index():
+    return 'Hello from Pet Adoption'
+
+
+@app.route('/adopt_puppy')
+def puppy():
+    return 'Do you want to take me home with you?'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
